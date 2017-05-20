ff4ee13e8e7968ca723803aa35b71edeb10d6439
Michał Ligęza <mligeziak@gmail.com>
2017-05-20 20:23:57 +0200

    add config to .gitignore

--- src/app/services/config.service.default.ts
+++ src/app/services/config.service.default.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-    apiUrl: string = 'http://flickapi.ct8.pl/';
+    apiUrl: string = 'http://movie.api.dev/';
 }
