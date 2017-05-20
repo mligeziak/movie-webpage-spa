02686351c50f96f41777f7e2fbd0c526d2bfa5c8
Michał Ligęza <mligeziak@gmail.com>
2017-05-20 20:03:55 +0200

    add config service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MoviesApiService } from './services/movies.api.service'
+import { ConfigService } from './services/config.service'
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [MoviesApiService]
+    providers: [
+        MoviesApiService,
+        ConfigService
+    ]
 })
 export class AppComponent { }
--- src/app/services/movies.api.service.ts
+++ src/app/services/movies.api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { ConfigService } from './config.service';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -7,14 +8,14 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MoviesApiService {
-    constructor(private http: Http) { }
+    constructor(private _http: Http, private _config: ConfigService) { }
 
     searchMovie(query: string): Observable<object> {
-        return this.http.get('http://movie.api.dev/movies/search/' + query + '.json').map(this.extractData).catch(this.handleError);
+        return this._http.get(this._config.apiUrl + 'movies/search/' + query + '.json').map(this.extractData).catch(this.handleError);
     };
 
     getMovieByImdbid(imdbid: string): Observable<object> {
-        return this.http.get('http://movie.api.dev/movies/getMovieByImdbid/' + imdbid + '.json').map(this.extractData).catch(this.handleError);
+        return this._http.get(this._config.apiUrl + 'movies/getMovieByImdbid/' + imdbid + '.json').map(this.extractData).catch(this.handleError);
     };
 
     private extractData(res: Response) {
