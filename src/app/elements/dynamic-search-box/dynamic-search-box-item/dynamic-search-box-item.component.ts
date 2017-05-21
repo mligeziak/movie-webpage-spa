66d0fb94a175161985060fbed85bacee744d7055
Michał Ligęza <mligeziak@gmail.com>
2017-05-21 12:33:10 +0200

    change dynamic search items view

--- src/app/elements/dynamic-search-box/dynamic-search-box-item/dynamic-search-box-item.component.ts
+++ src/app/elements/dynamic-search-box/dynamic-search-box-item/dynamic-search-box-item.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'dynamic-search-box-item',
-    templateUrl: './dynamic-search-box-item.component.html'
+    templateUrl: './dynamic-search-box-item.component.html',
+    styleUrls: ['./dynamic-search-box-item.component.scss']
 })
 export class DynamicSearchBoxItemComponent {
     @Input() movie: string;
