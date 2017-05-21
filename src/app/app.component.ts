a51a130b127b06edbfd5e209918b002eda5687bf
Michał Ligęza <mligeziak@gmail.com>
2017-05-21 20:06:46 +0200

    remove app.component empty scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { ConfigService } from './services/config.service'
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
     providers: [
         ApiService,
         ConfigService
